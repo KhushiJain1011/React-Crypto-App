3c5be131fba1aee21a56b6f68a685b13d185f798
Khushi Jain <khushij1011@gmail.com>
2024-10-01 22:19:22 +0530

    crypto app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,31 @@
 import { Avatar, Box, Stack, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
-import avatarSrc from "../assets/img1.jpg";
+import avatarSrc from "../assets/user.jpg";
 // const avatarSrc = ('./assets/img1.jpg');
 
 const Footer = () => {
   return (
     <Box
       bgColor={"blackAlpha.900"}
-      color={"whiteAlpha.700"}    
+      color={"whiteAlpha.700"}
       minH={"48"}
       px={"16"}
       py={["16", "8"]}
     >
-        <Stack
-          direction={["column", "row"]}
-          h={"full"}
-          alignItems={"center"}
-        >
-            <VStack w={"full"} alignItems={["center", "flex-start"]}>
-                <Text fontWeight={"bold"}>About Us</Text>
-                <Text fontSize={"sm"} letterSpacing={"widest"} textAlign={["center", "left"]}>We are the best crypto trading app in India. We provide our guidance at a very reasonable price. </Text>
-            </VStack>
-            <VStack>
-                <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc}/>
-                <Text>Our Founder</Text>
-            </VStack>
-        </Stack>
+      <Stack
+        direction={["column", "row"]}
+        h={"full"}
+        alignItems={"center"}
+      >
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text fontSize={"sm"} letterSpacing={"widest"} textAlign={["center", "left"]}>We are the best crypto trading app in India. We provide our guidance at a very reasonable price. </Text>
+        </VStack>
+        <VStack>
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
+          <Text>Our Founder</Text>
+        </VStack>
+      </Stack>
     </Box>
   )
 }
